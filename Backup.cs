f28e3c90052343df90fbf272cd6600ebba7b1681
Igor Kubacki <igorkubacki@gmail.com>
2022-10-16 17:40:44 +0200

    initial commit

---
+++ Backup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firebase_restore
+{
+    internal class Backup
+    {
+    }
+}
