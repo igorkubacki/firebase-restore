ce6938c27af245417b110f4aa65aa30b3e09ef02
Igor Kubacki <igorkubacki@gmail.com>
2022-10-18 01:01:48 +0200

    Fields now being saved to the backup file

--- Backup.cs
+++ Backup.cs
@@ -1,12 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Google.Cloud.Firestore;
+using System.Text.Json;
 
 namespace firebase_restore
 {
-    internal class Backup
+    static class Backup
     {
+        private static FirestoreDb db;
+        public static async void Start()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter the path to your service account key JSON file, then press enter: ");
+            string? path = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            await BackupData(path);
+        }
+
+        private async static Task BackupData(string path)
+        {
+            db = FirestoreDb.Create("availability-monitor-7231f");
+
+            // Getting all collections ID.
+            var collections = db.ListRootCollectionsAsync();
+
+            List<DataCollection> data = new List<DataCollection>();
+
+            await foreach(var coll in collections)
+            {
+                data.Add(new DataCollection(coll.Id, coll.Path));
+            }
+
+            foreach(DataCollection collection in data)
+            {
+                collection.Documents = await GetChildDocuments(collection);
+                
+            }
+
+
+            string json = JsonSerializer.Serialize(data);
+
+            File.WriteAllText("firestore_backup_" + DateTime.Now.ToShortDateString() + ".json", json);
+        }
+        private static async Task<List<DataDocument>> GetChildDocuments(DataCollection collection)
+        {
+            QuerySnapshot snapshot = await db.Collection(collection.Id).GetSnapshotAsync();
+
+            List<DataDocument> documents = new List<DataDocument>();
+
+            foreach (var documentSnapshot in snapshot)
+            {
+                Dictionary<string, object> document = documentSnapshot.ToDictionary();
+                DataDocument documentData = new DataDocument(documentSnapshot.Id, documentSnapshot.Reference.Path);
+
+                foreach (var key in document.Keys)
+                {
+                    documentData.Fields.Add(key, document[key]);
+                }
+
+                documents.Add(documentData);
+            }
+
+            return documents;
+        }
+        private static async Task GetDocumentCollections()
+        {
+            
+        }
+    }
+
+    class DataCollection
+    {
+        public string Id { get; set; }
+        public string Path { get; set; }
+        public List<DataDocument> Documents { get; set; }
+
+        public DataCollection(string id, string path)
+        {
+            Id = id;
+            Path = path;
+            Documents = new List<DataDocument>();
+        }
+    }
+
+    class DataDocument
+    {
+        public string Id { get; set; }
+        public string Path { get; set; }
+        public Dictionary<string, object> Fields { get; set; }
+        public List<DataCollection> Collections { get; set; }
+
+        public DataDocument(string id, string path)
+        {
+            Id = id;
+            Path = path;
+            Fields = new Dictionary<string, object>();
+            Collections = new List<DataCollection>();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace firebase_restore
+{
+    static class Firebase_Restore
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Make a backup");
+                Console.WriteLine("2. Restore a backup");
+                Console.WriteLine("3. Exit");
+                Console.Write("What would you like to do [1-3]:");
+                ConsoleKey input = Console.ReadKey().Key;
+
+                switch (input)
+                {
+                    case ConsoleKey.D1:
+                        Backup.Start();
+                        break;
+                    case ConsoleKey.D2:
+                        break;
+                    case ConsoleKey.D3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid input, try again.");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
